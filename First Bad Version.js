1f6ee1c931c8d4a41f88b0982f38fd910ffd6c2a
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-08-01 19:35:30 -0400

    August 1st complete

---
+++ First Bad Version.js
@@ -0,0 +1,18 @@
+// First Bad Version - Easy - August 1st
+
+function solution(isBadVersion){
+    return function(n){
+        let start = 1
+        let end = n
+
+        while (start <= end){
+            let mid = Math.floor((start + end) / 2)
+            if (!isBadVersion(mid)){
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+        return start
+    }
+}
